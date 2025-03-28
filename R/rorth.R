ba5c752dc7b43e52ac1c9c317343315928875246
Nordhausen <nordklau@ad.helsinki.fi>
2025-03-28 22:15:33 +0200

    Updating documentation and fixing rorth

--- R/rorth.R
+++ R/rorth.R
@@ -1,1 +1,5 @@
-rorth <- function(k) { qr.Q(qr(matrix(rnorm(k * k), k)))}
+rorth <- function(k) { 
+  temp <- qr(matrix(rnorm(k * k), k))
+  sign_vec <- sign(diag(qr.R(temp)))
+  sweep(qr.Q(temp), 2, sign_vec, "*")
+  }
